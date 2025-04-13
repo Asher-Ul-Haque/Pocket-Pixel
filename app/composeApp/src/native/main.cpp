dec764fbd03f32cd497fd5f4734a47eea6d1a1fd
Sak-drago <sakshatsachdeva@gmail.com>
2025-04-13 18:37:33 +0530

    Task Update: [+] MORE FIXES

--- app/composeApp/src/native/main.cpp
+++ app/composeApp/src/native/main.cpp
@@ -1,8 +1,8 @@
 #ifndef __ANDROID__
-#include "../ForgeLib/include/logger.h"
-#include "../GameBoyCore.h"
-#include "../Machine/Parts/emu.h"
-#include "../Machine/Parts/cartridge.h"
+#include "ForgeLib/include/logger.h"
+#include "GameBoyCore.h"
+#include "Machine/Parts/emu.h"
+#include "Machine/Parts/cartridge.h"
 
 int main(int argc, char const *argv[])
 {
