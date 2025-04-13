8838052756afc2e602338b72a9447f9337c4795f
Sak-drago <sakshatsachdeva@gmail.com>
2025-04-13 15:46:15 +0530

    Task Update: [=] Minor Fix

--- app/composeApp/src/native/Machine/Utils/bit.h
+++ app/composeApp/src/native/Machine/Utils/bit.h
@@ -7,7 +7,7 @@ extern "C" {
 // - - - This file contains bitwise functions for help
 
 // - - - check if the n'th bit of an integer is high or low
-#define IS_BIT_SET(NUMBER, BIT_POSITION) ((NUMBER & (1 << BIT_POSITION)) ? true : false);
+#define IS_BIT_SET(NUMBER, BIT_POSITION) ((NUMBER & (1 << BIT_POSITION)) ? true : false)
 // - - - set the n'th bit of an integer to on
 #define SET_BIT(NUMBER, BIT_POSITION, ON) {if(ON) (NUMBER) |= (1 << BIT_POSITION); else (NUMBER) &= ~(1 << BIT_POSITION);}
 // - - - check if it lies between two bits
